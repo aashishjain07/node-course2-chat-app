c848078252d66963f8bc6ffa3edbec2e9523a458
Ashish Jain <Ashish.jain8847@gmail.com>
2018-07-25 13:09:32 +0530

    Emit newMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,13 +2,7 @@ var socket = io();
 
 socket.on('connect', function() {
 console.log('Connected to Server');
-
-socket.emit('createMessage',{
-    from: 'Ashish',
-    text: 'Yup, that works for me'
 });
-});
-
 socket.on('disconnect', function() {
     console.log('Disconnected from server');
 });
